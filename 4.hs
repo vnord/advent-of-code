b3f3feb934c300a2f77974918f140d0c2d7d3130
vnord <admin@vnord.net>
2019-12-04 10:26:30 +0100

    complete part 2 of day 4

--- 4.hs
+++ 4.hs
@@ -1,3 +1,5 @@
+import Data.List
+
 monotonicDigits :: Int -> Bool
 monotonicDigits = monotonicString . show
 
@@ -8,9 +10,20 @@ monotonicString (_:[]) = True
 hasDoubleDigits :: Int -> Bool
 hasDoubleDigits = hasDoubleChars . show
 
+hasDoubleDigits' :: Int -> Bool
+hasDoubleDigits' = hasDoubleChars' . show
+
 hasDoubleChars :: String -> Bool
-hasDoubleChars (x:y:xs) = (x == y) || hasDoubleChars (y:xs)
+hasDoubleChars (x:y:xs) = x == y || hasDoubleChars (y:xs)
 hasDoubleChars (_:[]) = False
 
+hasDoubleChars' :: String -> Bool
+hasDoubleChars' = not . null . filter (\x -> length x == 2) . group
+
 part1 :: Int
-part1 = length $ filter hasDoubleDigits $ filter monotonicDigits [138241 .. 674034]
+part1 = length $ filter hasDoubleDigits
+               $ filter monotonicDigits [138241 .. 674034]
+
+part2 :: Int
+part2 = length $ filter hasDoubleDigits' 
+               $ filter monotonicDigits [138241 .. 674034]
