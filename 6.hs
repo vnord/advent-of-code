5ff9ecac8fa0fcb24d68d39a2d1a4f2868068915
vnord <admin@vnord.net>
2019-12-06 15:29:12 +0100

    solve part 1 of day 6

--- 6.hs
+++ 6.hs
@@ -1,13 +1,22 @@
 import Data.List
 import Data.List.Split
 import qualified Data.MultiMap as MM
-import Data.Tree.Class
-import Data.Tree.NTree.TypeDefs
 
 
 main = do
- x <- MM.fromList . map pairify . lines <$> readFile "6-example.txt"
- print "6"
+ x <- MM.fromList . map pairify . lines <$> readFile "6.txt"
+ print $ orbits x
+
+parents = do
+    x <- getMap
+    pure $ MM.keys x
+
+findAllChildren :: MM.MultiMap String String -> String -> [String]
+findAllChildren m e = kids ++ (concatMap (findAllChildren m) kids)
+  where kids = MM.lookup e m
+
+orbits :: MM.MultiMap String String -> Int
+orbits m = length . concat $ [findAllChildren m e | e <- MM.keys m]
 
 getMap :: IO (MM.MultiMap String String)
 getMap = MM.fromList . map pairify . lines <$> readFile "6-example.txt"
