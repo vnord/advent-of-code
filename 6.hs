377c8ca6235b8f61e94f2a4996293661d658a39e
vnord <admin@vnord.net>
2019-12-06 16:20:59 +0100

    solve part 2 of day 6

--- 6.hs
+++ 6.hs
@@ -1,25 +1,26 @@
 import Data.List
+import Data.Ord
 import Data.List.Split
 import qualified Data.MultiMap as MM
 
-
 main = do
  x <- MM.fromList . map pairify . lines <$> readFile "6.txt"
+ let i = filter (isUseful x) $ minimumBy (comparing length)
+            $ filter (any (=="YOU")) $ filter (any (=="SAN"))
+            [findAllChildren x e | e <- MM.keys x]
  print $ orbits x
-
-parents = do
-    x <- getMap
-    pure $ MM.keys x
+ print $ length i
 
 findAllChildren :: MM.MultiMap String String -> String -> [String]
 findAllChildren m e = kids ++ (concatMap (findAllChildren m) kids)
   where kids = MM.lookup e m
 
+isUseful :: MM.MultiMap String String -> String -> Bool
+isUseful m e = any (== "YOU") kids || any (== "SAN") kids
+  where kids = findAllChildren m e
+
 orbits :: MM.MultiMap String String -> Int
 orbits m = length . concat $ [findAllChildren m e | e <- MM.keys m]
 
-getMap :: IO (MM.MultiMap String String)
-getMap = MM.fromList . map pairify . lines <$> readFile "6-example.txt"
- 
 pairify :: String -> (String, String)
 pairify = (\(x:y:[]) -> (x,y)) . splitOn ")"
