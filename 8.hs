5b513863bdd331d36f67deeddf8eb513daf0c1c3
vnord <admin@vnord.net>
2019-12-08 14:22:44 +0100

    cleanup

--- 8.hs
+++ 8.hs
@@ -3,13 +3,9 @@ import Data.List.Split
 
 main = do
   input <- chunksOf (25*6) . init <$> readFile "8.txt"
-  print (length input, input)
   let (c, l) = minimum . map (\x -> (length (filter (=='0') x), x)) $ input
-  print (c, l)
-  let answer = length ((filter (=='1')) l) * length ((filter (=='2')) l)
-  print answer
-  let vs =  map findValue $ transpose input
-  mapM_ print $ paintPixels vs
+  print $  length (filter (=='1') l) * length (filter (=='2') l)
+  mapM_ print $ paintPixels . map findValue $ transpose input
 
 findValue :: [Char] -> Char 
 findValue ('2':cs) = findValue cs
